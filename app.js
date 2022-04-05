6c1a488a137f62e488bc339080e23e6e8242e997
Joey Huntington <jwhunt19@gmail.com>
2022-04-04 23:25:38 -0600

    clean up per eslint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
 import { Client, Intents } from 'discord.js';
-import { buildCommand } from './helper.js';
 import dotenv from 'dotenv';
+import { buildCommand } from './helper.js';
 
 dotenv.config();
 
@@ -9,47 +11,47 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const prefix = '!';
 
 client.on('ready', async () => {
-  console.log('share bot is online!')
-  
+  console.log('share bot is online!');
+
   const ch = await client.channels.fetch(process.env.CHANNEL);
   if (ch.isText) {
     ch.send('I AM ONLINE');
-  } 
+  }
 });
 
 // bot commands
 client.on('messageCreate', async (msg) => {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
-  
+
   // send custom message to specific channel
-  if (msgContent.startsWith('!send')) {
-    let msgContent = msg.content
-    let msgAuthor = msg.author.id
+  if (msg.content.startsWith('!send')) {
+    let msgContent = msg.content;
+    let msgAuthor = msg.author.id;
 
     // check if reply
     if (msg.type === 'REPLY') {
-      const messageBody = await (await msg.channel.messages.fetch(msg.reference.messageId)).content
-      const request = msgContent.split(' ')
+      const messageBody = await (await msg.channel.messages.fetch(msg.reference.messageId)).content;
+      const request = msgContent.split(' ');
 
-      if (request.length > 3) return
+      if (request.length > 3) return;
 
-      msgContent = `${request[0]} ${messageBody} ${request[1]} ${request[2]}`
-      msgAuthor = msg.mentions.repliedUser.id
+      msgContent = `${request[0]} ${messageBody} ${request[1]} ${request[2]}`;
+      msgAuthor = msg.mentions.repliedUser.id;
     }
 
     // channel query check
-    const chQuery = msgContent.match(/^!send\s.*\sto\s(.*)/)[1]
+    const chQuery = msgContent.match(/^!send\s.*\sto\s(.*)/)[1];
     if (!chQuery.match(/^<#\d+>$/)) {
-      const ch = client.channels.cache.filter(channel => channel.name === chQuery).map(channel => channel.id)
-      
+      const ch = client.channels.cache.filter((c) => c.name === chQuery).map((c) => c.id);
+
       if (ch.length > 0) {
-        msgContent = `${msgContent.match(/^(!send\s.*\sto\s).*$/)[1]}<#${ch[0]}>`
+        msgContent = `${msgContent.match(/^(!send\s.*\sto\s).*$/)[1]}<#${ch[0]}>`;
       }
     }
 
     // build message and designated channel array
-    const [message, channel] = buildCommand(msgContent)
+    const [message, channel] = buildCommand(msgContent);
 
     // send message to designated channel if text channel
     if (message) {
@@ -58,7 +60,7 @@ client.on('messageCreate', async (msg) => {
         ch.send(`<@!${msgAuthor}> says ${message}`);
       }
     } else {
-      msg.reply('there seems to be an error in your request, please try again.')
+      msg.reply('there seems to be an error in your request, please try again.');
     }
   }
 });
--- helper.js
+++ helper.js
@@ -1,13 +1,15 @@
+/* eslint-disable import/prefer-default-export */ // DELETE
+/* eslint-disable no-console */
 function buildCommand(text) {
   const reg = /^!send\s(.*)\sto\s<#(\d+)>$/;
   const ms = text.match(reg);
 
   if (!ms) {
-      console.warn('no matches found');
+    console.warn('no matches found');
     return [null, null];
   }
-  
-  return [ms[1], ms[2]]
+
+  return [ms[1], ms[2]];
 }
 
-export { buildCommand }
+export { buildCommand };
