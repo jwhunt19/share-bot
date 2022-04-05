f4193410d7e62156f934c8e02db9b1492ac7fd7f
Joey Huntington <jwhunt19@gmail.com>
2022-04-04 23:11:08 -0600

    add channel query option and send by reply function

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 import { Client, Intents } from 'discord.js';
+import { buildCommand } from './helper.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const prefix = '!'
+const prefix = '!';
 
 client.on('ready', async () => {
   console.log('share bot is online!')
   
-  const ch = await client.channels.fetch('958233169247760455');
+  const ch = await client.channels.fetch(process.env.CHANNEL);
   if (ch.isText) {
     ch.send('I AM ONLINE');
-  }
-  
-});
-
-// responds to any message in the test channel, tagging the author
-client.on('messageCreate', (msg) => {
-  if (msg.author.bot) return;
-  
-  if (msg.channelId === '958943290848321536') {
-    msg.reply(`<@!${msg.author.id}> said "${msg.content}" \n\n I think that's a cool message`)
-  }
+  } 
 });
 
 // bot commands
@@ -31,30 +22,43 @@ client.on('messageCreate', async (msg) => {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
   
-  let msgContent = msg.content.toLocaleLowerCase()
-  
   // send custom message to specific channel
   if (msgContent.startsWith('!send')) {
-    // build command array
-    function buildCommand(text) {
-      const reg = /^!send\s(.*)\sto\s<#(\d+)>$/;
-      const ms = text.match(reg);
+    let msgContent = msg.content
+    let msgAuthor = msg.author.id
 
-      if (!ms) {
-          console.warn('no matches found');
-        return;
-      }
-      
-      return [ms[1], ms[2]]
+    // check if reply
+    if (msg.type === 'REPLY') {
+      const messageBody = await (await msg.channel.messages.fetch(msg.reference.messageId)).content
+      const request = msgContent.split(' ')
+
+      if (request.length > 3) return
+
+      msgContent = `${request[0]} ${messageBody} ${request[1]} ${request[2]}`
+      msgAuthor = msg.mentions.repliedUser.id
     }
 
-    const [message, channel] = buildCommand(msg.content)
-    
-    // TODO - add error handling, text search instead of direct channel id using this func
-    // const ch = client.channels.cache.filter(channel => channel.name === "test-zone").map(channel => channel.id)
-    const ch = await client.channels.fetch(channel);
-    if (ch.isText) {
-      ch.send(`<@!${msg.author.id}> says ${message}`);
+    // channel query check
+    const chQuery = msgContent.match(/^!send\s.*\sto\s(.*)/)[1]
+    if (!chQuery.match(/^<#\d+>$/)) {
+      const ch = client.channels.cache.filter(channel => channel.name === chQuery).map(channel => channel.id)
+      
+      if (ch.length > 0) {
+        msgContent = `${msgContent.match(/^(!send\s.*\sto\s).*$/)[1]}<#${ch[0]}>`
+      }
+    }
+
+    // build message and designated channel array
+    const [message, channel] = buildCommand(msgContent)
+
+    // send message to designated channel if text channel
+    if (message) {
+      const ch = await client.channels.fetch(channel); // FIXME handle channel permission errors
+      if (ch.isText) {
+        ch.send(`<@!${msgAuthor}> says ${message}`);
+      }
+    } else {
+      msg.reply('there seems to be an error in your request, please try again.')
     }
   }
 });
