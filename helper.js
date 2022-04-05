6d1bf52d7c007723b2ac3e59c662f8c5719c3812
Joey Huntington <jwhunt19@gmail.com>
2022-04-04 23:10:35 -0600

    move build command function to helper file

---
+++ helper.js
@@ -0,0 +1,13 @@
+function buildCommand(text) {
+  const reg = /^!send\s(.*)\sto\s<#(\d+)>$/;
+  const ms = text.match(reg);
+
+  if (!ms) {
+      console.warn('no matches found');
+    return [null, null];
+  }
+  
+  return [ms[1], ms[2]]
+}
+
+export { buildCommand }
